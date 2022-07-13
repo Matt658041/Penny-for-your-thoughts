76263201b31cf47ac4a5725cffacbd72a382fcbf
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-13 10:47:32 -0500

    adding quote images to seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -21,7 +21,7 @@ db.once('open', async () => {
       name: 'Tin of Cookies',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      image: 'confucious.jpg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 500
@@ -30,17 +30,17 @@ db.once('open', async () => {
       name: 'Canned Coffee',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      image: 'hepburn.jpg',
       category: categories[0]._id,
       price: 1.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
+      name: 'Margaret Mead',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+        'Always remember that you are absolutely unique. Just like everyone else.',
+      image: 'margaretmead1.jpg',
       price: 7.99,
       quantity: 20
     },
