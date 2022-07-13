6f632a4ae9ef1ebcf779d05cfb006d538e063caf
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-13 11:16:18 -0500

    still adding quotes to seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -18,21 +18,21 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Confucius',
       description:
-        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+        'Everything has beauty, but not everyone sees it.',
       image: 'confucius.jpg',
       category: categories[0]._id,
-      price: 2.99,
+      price: 2000.99,
       quantity: 500
     },
     {
-      name: 'Canned Coffee',
+      name: 'Audrey Hepburn',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+        'Nothing is impossible the word itself says I am possible.',
       image: 'hepburn.jpg',
       category: categories[0]._id,
-      price: 1.99,
+      price: 1000.99,
       quantity: 500
     },
     {
@@ -41,7 +41,7 @@ db.once('open', async () => {
       description:
         'Always remember that you are absolutely unique. Just like everyone else.',
       image: 'margaretmead1.jpg',
-      price: 7.99,
+      price: 7000.99,
       quantity: 20
     },
     {
@@ -50,24 +50,24 @@ db.once('open', async () => {
       description:
         'Start where you are. Use what you have. Do what you can.',
       image: 'life-changing.jpg',
-      price: 3.99,
+      price: 300.99,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
+      name: 'Brene Brown',
       category: categories[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+        'Courage starts with showing up an letting ourselves be seen.',
+      image: 'brene.jpg',
       price: 14.99,
       quantity: 100
     },
     {
-      name: 'Camera',
+      name: 'Albert Einstein',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
+        'Life is like riding a bicycle To keep your balance, you must keep moving.',
+      image: 'albert.jpg',
       price: 399.99,
       quantity: 30
     },
