3e220a63a72e15ec1db5cefd51e77e0b9dbed3ea
Kimberly West <kdenton993@gmail.com>
2022-07-13 19:26:31 -0600

    added media breaking points for mobile first design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./client", "./server"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+    },
   },
   plugins: [],
 }
