e029ff01e4ab7861731ceeb2d6a3d9cacadfa500
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-14 15:05:16 -0500

    created a component for quotes

---
+++ client/src/components/Quotes/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Quotes extends React.Component {
+    state ={
+        persons: []
+    }
+    componentDidMount() {
+        axios.get('https://world-of-quotes.p.rapidapi.com/v1/quotes/quote-of-the-day')
+        .then(res =>{
+            const quotes = res.data;
+            this.setState({ quotes })
+        })
+    }
+    render() {
+        return (
+        <ul>
+            {
+                this.state.quotes
+                .map(quote => 
+                    <li key= {quote.id}>{quote.name}</li>)
+            }
+
+        </ul>
+        )
+    }
+}
