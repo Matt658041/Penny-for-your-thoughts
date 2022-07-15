d5dac9a320f026034562874b292674f42a6f6706
Matthew Songer <matt91893@gmail.com>
2022-07-15 15:55:21 -0500

    get boostrap to work

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -40,7 +40,7 @@ function ProductItem(item) {
   }
 
   return (
-    <div className="card px-1 py-1">
+    <div className="card px-1">
       <Link to={`/products/${_id}`}>
         <img
           alt={name}
@@ -52,7 +52,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button onClick={addToCart}>Add to cart</button>
+      <button className="btn-primary" onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
