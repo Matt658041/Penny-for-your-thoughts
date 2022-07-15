9e82c155ebf590a3c95c59401ab0104930851b69
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-15 13:59:37 -0500

    moved api js to pages folder

--- client/src/pages/API.js
+++ client/src/pages/API.js
@@ -1,9 +1,11 @@
+import React from "react";
+
 export const axios = require("axios");
 
 const options = {
   method: 'GET',
   url: 'https://world-of-quotes.p.rapidapi.com/v1/quotes/quote-of-the-day',
-  params: {category: 'inspirational'},
+ 
   headers: {
     'X-RapidAPI-Key': '6744f82751mshc3605f6521c8a9cp1ebe17jsn36d4d832059b',
     'X-RapidAPI-Host': 'world-of-quotes.p.rapidapi.com'
