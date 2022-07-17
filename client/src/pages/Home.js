c2676b32a4e9d3716e0542e4915a68f1363fca1e
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2022-07-17 18:45:24 -0400

    added quote api integration

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -44,8 +44,7 @@ console.log("line25hit");
       <div class="box">
   <img src="https://imgix.bustle.com/rehost/2017/8/21/d82f3279-8ae5-47cb-8ed0-bef83fd34033.jpg" alt="An open book" width="1200" height="500" />
   <div class="shade"></div>
-  <j>THIS IS WHERE I'M GONNA PUT 
-    <br />SOMETHING NICE TO SAY</j>
+  <div>{quote}</div>
 </div>
       <br />
       <br /><center>
