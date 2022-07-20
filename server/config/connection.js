e68112446cff1614987104a426ebeb83e023dd72
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-07-20 18:03:36 -0500

    fixed seeding problem

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
+mongoose.connect("mongodb+srv://Matt658041:Gateway3@cluster0.cq8fpb2.mongodb.net/Penny-for-your-thoughts?retryWrites=true&w=majority",{ //process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
